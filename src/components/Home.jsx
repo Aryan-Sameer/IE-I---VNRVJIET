34afe4424aab0af6524964929552549a44a6e638
Aryan-Sameer <sameeraryan2005@gmail.com>
2024-09-11 22:31:06 +0530

    explore to about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,9 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
 
-    const handleScroll = () => {
-        window.scrollTo({
-            top: window.innerHeight,
-            behavior: 'smooth'
-        })
+    const handleExplore = () => {
+        const about = document.getElementById("about")
+        about.scrollIntoView({ behavior: "smooth" })
     }
 
     useEffect(() => {
@@ -31,7 +29,7 @@ const Home = () => {
                         </div>
                         <div>
                             <h2 style={{ userSelect: "none" }}>Focusing on <span className='domain'></span></h2>
-                            <button className='explore' onClick={handleScroll}>Explore</button>
+                            <button className='explore' onClick={handleExplore}>Explore</button>
                         </div>
                     </div>
 
@@ -41,13 +39,13 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className='aboutUs'>
+            <div id='about' className='aboutUs'>
                 <div className="aboutInfo">
                     <h1 className='headings'>About us</h1>
                     <p>
                         The Institution of Engineers (India), IE(I), is the national organization for engineers in India and the largest multi-disciplinary engineering society in the world. This chapter enables students to apply technical knowledge gained from guest lectures, workshops, and project exhibitions. It fosters critical thinking and leadership skills, supporting budding engineers in their career growth. IEI conducts various student activities, with the annual technical fest "EmeRgE" being the highlight, blending fun and learning. The vision is to develop a premier technical and scientific institution, producing creative, skilled individuals, and to promote technology that enhances humanity.
                     </p>
-                    <Link style={{textDecoration: "none"}} to="/Team"><button className='ourTeamBtn'>Our Team <span className='teamArrow'><IoIosArrowForward /></span></button></Link>
+                    <Link style={{ textDecoration: "none" }} to="/Team"><button className='ourTeamBtn'>Our Team <span className='teamArrow'><IoIosArrowForward /></span></button></Link>
                 </div>
                 <div className="aboutImg">
                     <img src="/static/aboutImg.jpg" alt="" />
@@ -57,12 +55,12 @@ const Home = () => {
                             <h2>10+</h2>
                             <span>years of experience</span>
                         </div>
-                        <div className="line" style={{border: "2px solid black"}}></div>
+                        <div className="line" style={{ border: "2px solid black" }}></div>
                         <div className="s2">
                             <h2>15+</h2>
                             <span>events annually</span>
                         </div>
-                        <div className="line" style={{border: "2px solid black"}}></div>
+                        <div className="line" style={{ border: "2px solid black" }}></div>
                         <div className="s3">
                             <h2>350+</h2>
                             <span>Active members</span>
