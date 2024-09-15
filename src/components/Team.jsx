21863de431b86208f7ddae0fcdd87d8b391f2e8d
Aryan-Sameer <sameeraryan2005@gmail.com>
2024-09-15 22:52:45 +0530

    added photos to team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,42 +7,42 @@ const Team = () => {
     const members = [
         {
             id: 1,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/VAMSI.jpg",
             name: "M Vamsi Krishna ",
             position: "Chairman",
             email: "vamshimuniganti23@gmail.com"
         },
         {
             id: 2,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/sindhu.jpg",
             name: "G Sindhu Sri",
             position: "Vice Chair",
             email: "sindhusri1703@gmail.com"
         },
         {
             id: 3,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/sumanth.jpg",
             name: "Muniganti Sumanth",
             position: "General Secretary",
             email: "sumanthmuniganti31@gmail.com"
         },
         {
             id: 4,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/shankar.jpg",
             name: "L Bhavani Shankar Prasad",
             position: "President",
             email: "bhavanishankarr2003@gmail.com"
         },
         {
             id: 5,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/Prathiba.jpg",
             name: "Boddupalli Prathibha",
             position: "Vice President",
             email: "boddupalliprathiba@gmail.com"
         },
         {
             id: 6,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/ritu raj.jpg",
             name: "Rituraj Swain",
             position: "Operations Chief",
             email: "riturajswain30@gmail.com"
@@ -56,14 +56,14 @@ const Team = () => {
         },
         {
             id: 8,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/Sravya.jpg",
             name: "Sravya Vengala",
             position: "Technical Head",
             email: "vengalasravya41@gmail.com"
         },
         {
             id: 9,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/Meenakshi.jpg",
             name: "V Meenakshi",
             position: "Creative Head",
             email: "meenakshivannala3110@gmail.com"
@@ -77,14 +77,14 @@ const Team = () => {
         },
         {
             id: 11,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/aashrith.jpg",
             name: "Aashrith G",
             position: "Chief Event Organizer",
             email: "aashrithgunti@gmail.com"
         },
         {
             id: 12,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/Hrushikesh.jpg",
             name: "Hrushikesh Sarma",
             position: "PR Head",
             email: "hrushikeshsarma44@gmail.com"
@@ -98,21 +98,21 @@ const Team = () => {
         },
         {
             id: 14,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/Uday.jpg",
             name: "T Uday Venkat",
             position: "Inventory and Logistics",
             email: "udayvenkat2003@gmail.com"
         },
         {
             id: 15,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/Thirumal.jpg",
             name: "G Thirumal Chowdary",
             position: "Head Alumni Connect",
             email: "thirumalg98@gmail.com"
         },
         {
             id: 16,
-            image: "http://placehold.it/150x150",
+            image: "/static/core team/venkat.jpg",
             name: "P Y Venkata Sai Teja",
             position: "Executive Head",
             email: "venkatasai0204@gmail.com"
@@ -131,7 +131,7 @@ const Team = () => {
                 {members.map((item) => {
                     return (
                         <div key={item.id} className="memberId">
-                            <img src={item.image} alt="" />
+                            <img src={item.image} width={"150px"} height={"150px"} alt="" />
                             <div className="memberDesc">
                                 <h5>{item.name}</h5>
                                 <h6>{item.position}</h6>
