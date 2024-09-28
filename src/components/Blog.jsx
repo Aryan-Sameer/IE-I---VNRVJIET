47c0c281a1c493545d486b9365fdee7c6939cb14
Aryan-Sameer <sameeraryan2005@gmail.com>
2024-09-28 17:33:04 +0530

    changes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useEffect } from 'react'
 import "../css/blog.css"
 
 const Blog = () => {
 
     let isContestThere = true
-    
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
